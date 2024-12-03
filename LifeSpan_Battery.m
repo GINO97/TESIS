0f80e6f247de63822d0834d1cee92c3d4448c241
GINO97 <71912874+GINO97@users.noreply.github.com>
2024-12-03 15:04:39 -0500

    Update LifeSpan_Battery.m

--- LifeSpan_Battery.m
+++ LifeSpan_Battery.m
@@ -13,12 +13,18 @@ clear all, clc;
 % Duracion(1,i)=25160/E(1,i); %veces/h
 % Dias(1,i)=Duracion(1,i)/120/24;
 % end
-BW=250E3;
-SF=10;
-L=13; %bytes
-CR=6;
-Rb = SF*BW/(2^(SF))*4/(4+CR)
-Ptx=L*8*1/Rb;
-E= 3.7*123*Ptx*0.001
 
-Dias=(6.8*3.7/E)/4/24 % 4 muestras por hora = 60min/15min
+BW=62.5E3;
+SF=8;
+L=13; %bytes
+CR=5;
+Rb = (SF*BW)/(2^(SF))*(4/(4+CR));
+Ptx=L*8*1/Rb;
+
+Eb=6.8*3600*3.7;% E. batería (As)
+ETx= 3.7*151.5*0.001*Ptx % E. batería(As) ; 123 mA consumo aprox. del modulo
+
+%% tasa de datos
+
+Dias=((Eb/ETx)/15/3600)/24 % 
+horas =(Eb/ETx)/15/3600 %  15s/h/dia
